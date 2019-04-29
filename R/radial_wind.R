790a264f9ed4bc8ebd43ca6f6642f0436643fda0
Elio Campitelli <eliocampitelli@gmail.com>
2019-04-28 21:12:42 -0300

    Documenta datos y edita vad_fit

---
+++ R/radial_wind.R
@@ -0,0 +1,16 @@
+#' One volume of radial velocity
+#'
+#' A single volume of radial velocity measured on January 14th 2016 between
+#' 06:00:05 and 06:04:31 at Paraná, Entre Ríos.
+#'
+#' @format A `data.table` with 2076960 rows and 4 variables:
+#' \describe{
+#'   \item{range}{distance to de radar in meters}
+#'   \item{vr}{radial velocity in m/s}
+#'   \item{azimuth}{azimuth angle in degrees}
+#'   \item{elevation}{elevation angle in degrees}
+#' }
+#'
+#'
+#'@source https://radar.inta.gob.ar/
+"radial_wind"
